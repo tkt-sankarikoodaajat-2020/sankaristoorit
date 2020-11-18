8a8b9a9a7abaab9002d38e5a48db4fa0d96c1334
johanneslares <Johq.lares@gmail.com>
2020-11-18 21:41:38 +0200

    Edited backend not to crash

--- sankaristoorit-backend/app.js
+++ sankaristoorit-backend/app.js
@@ -3,6 +3,7 @@ const tipsRouter = require('./routers/ReadingTip')
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const errors = require('./utils/errors')
 const app = express()
 
 console.log('connecting to MongoDB: ' + conf.MONGODB_URI)
@@ -14,15 +15,12 @@ mongoose.connect(conf.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: 
     console.log('error connecting to MongoDB: ', error.message)
   })
 
-app.use(express.static('build'))
 app.use(express.json())
 
 app.use(cors())
 
 app.use('/tips', tipsRouter)
 
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/build/index.html'));
-});
+app.use(errors.unknownEndpoint)
 
 module.exports = app
