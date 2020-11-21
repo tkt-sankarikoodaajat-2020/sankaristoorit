4a76dd22962bc318953d125f96d0bf7fa15a70b5
Teemu Närhi <t.a.narhi@gmail.com>
2020-11-21 23:48:18 +0200

    added e2e-testing to tip creation and deletion

--- sankaristoorit-ui/cypress/integration/tips_app.spec.js
+++ sankaristoorit-ui/cypress/integration/tips_app.spec.js
@@ -14,4 +14,20 @@ describe('Tips app ', function () {
     cy.contains('Tips')
     cy.contains('First')
   })
+
+  it('Creating a tip works correctly', function() {
+    cy.contains('Create').click()
+    cy.get('#title').type('Cy_testi')
+    cy.get('#create-button').click()
+
+    cy.contains('Cy_testi')
+  })
+
+  it('Deleting a tip works correctly', function() {
+    cy.contains('Cy_testi').within(() => {
+      cy.contains('Delete').click()
+    })
+    cy.visit('http://localhost:3000')
+    cy.contains('Cy_testi').should('not.exist')  
+  })
 })
