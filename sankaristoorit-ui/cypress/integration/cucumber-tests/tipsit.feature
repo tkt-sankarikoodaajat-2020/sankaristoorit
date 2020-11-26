344787198cf14437fd2289243e09e97b873057fb
StrappedGlint13 <matiasbrax@gmail.com>
2020-11-26 17:33:37 +0200

    cucumbers for handling tips

--- sankaristoorit-ui/cypress/integration/cucumber-tests/tipsit.feature
+++ sankaristoorit-ui/cypress/integration/cucumber-tests/tipsit.feature
@@ -1,7 +1,26 @@
-Feature: I want to add tips
+Feature: I want to manage tips
 
 Scenario: Adding a tip
 
 Given I am on the front page
 When I enter information about a tip
 Then a tip is created
+
+Scenario: Adding a tip fails without a title
+
+Given I am on the front page
+When I enter a tip without a title
+Then a tip without a title is not added
+
+Scenario: Adding a tip with bad url-format
+
+Given I am on the front page
+When I enter a tip with bad url-format
+Then a tip is not added
+
+Scenario: Deleting a tip
+
+Given I am on the front page
+When I press delete on a tip
+Then a tip is deleted
+
--- sankaristoorit-ui/cypress/integration/cucumber-tests/tipsit/tipsit.js
+++ sankaristoorit-ui/cypress/integration/cucumber-tests/tipsit/tipsit.js
@@ -7,19 +7,44 @@ Given('I am on the front page', () => {
 When('I enter information about a tip', () => {
   cy.contains('Create').click()
   cy.get('#title').type('Cy_testi')
+  cy.get('#url').type('https://fullstackopen.com/osa1/monimutkaisempi_tila_reactin_debuggaus')
+  cy.get('#create-button').click()
+})
+
+When('I enter a tip without a title', () => {
+  cy.contains('Create').click()
+  cy.get('#create-button').click()
+})
+
+When('I enter a tip with bad url-format', () => {
+  cy.contains('Create').click()
+  cy.get('#title').type('Cy_testi')
+  cy.get('#url').type('hps://fullstackopen.com/osa1/monimutkaisempi_tila_reactin_debuggaus')
   cy.get('#create-button').click()
 })
 
 When('I press delete on a tip', () => {
-  cy.contains('Cy_testi').within(() => {
+  cy.contains('Create').click()
+  cy.get('#title').type('Delete_testi')
+  cy.get('#create-button').click()
+  cy.contains('Delete_testi').within(() => {
     cy.contains('Delete').click()
   })
 })
 
 Then('a tip is created', () => {
   cy.contains('Cy_testi')
+  cy.contains('https://fullstackopen.com/osa1/monimutkaisempi_tila_reactin_debuggaus')
+})
+
+Then('a tip without a title is not added', () => {
+  cy.contains('title missing')
+})
+
+Then('a tip is not added', () => {
+  cy.contains('url is incorrect')
 })
 
 Then('a tip is deleted', () => {
-  cy.contains('Cy_testi').should('not.exist')
+  cy.contains('Delete_testi').should('not.exist')
 })
