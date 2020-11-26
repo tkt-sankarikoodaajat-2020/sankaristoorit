beaa13ad2e8098db10524f17d1538cfcc546dc0e
StrappedGlint13 <matiasbrax@gmail.com>
2020-11-26 10:16:46 +0200

    url view added to frontend, jest test checks url, E2E tests written for tip-adding

--- sankaristoorit-ui/cypress/integration/tips_app.spec.js
+++ sankaristoorit-ui/cypress/integration/tips_app.spec.js
@@ -12,14 +12,30 @@ describe('Tips app ', function () {
 
   it('Tips are rendered correclty', function () {
     cy.contains('Tips')
-    cy.contains('First')
   })
 
-  it('Creating a tip works correctly', function () {
+  it('Creating a tip works correctly with valid data', function () {
     cy.contains('Create')
     cy.get('#title').type('Cy_testi2')
+    cy.get('#url').type('https://github.com/tkt-sankarikoodaajat-2020/sankaristoorit')
     cy.get('#create-button').click()
     cy.contains('Cy_testi2')
+    cy.contains('https://github.com/tkt-sankarikoodaajat-2020/sankaristoorit')
+  })
+
+  it('Creating a tip fails with incorrect url', function () {
+    cy.contains('Create')
+    cy.get('#title').type('Cy_testi2')
+    cy.get('#url').type('hps://github.com/tkt-sankarikoodaajat-2020/sankaristoorit')
+    cy.get('#create-button').click()
+    cy.contains('url is incorrect')
+  })
+
+  it('Creating a tip fails without title with correct url', function () {
+    cy.contains('Create')
+    cy.get('#url').type('https://github.com/tkt-sankarikoodaajat-2020/sankaristoorit')
+    cy.get('#create-button').click()
+    cy.contains('title missing')
   })
 
   it('Deleting a tip works correctly', function () {
--- sankaristoorit-ui/src/components/Tip.js
+++ sankaristoorit-ui/src/components/Tip.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Section, Button } from 'react-bulma-components'
 
+const UrlOrNot = ( { url } ) => {
+  if (url !== null) {
+    return <p className='message-body'>{url}</p>
+  }
+  return <p className='message-body'></p>
+}
+
 const Tip = ({ tip, deleteTip }) => {
 
   return (
     <Section>
       <li className='message is-primary'>
         <h2 className='message-header'>{tip.title}<Button className='delete' onClick={() => deleteTip(tip.id)}>Delete</Button></h2>
-        <p className='message-body'>message placeholder</p>
+        <UrlOrNot url={tip.url} />
       </li>
     </Section>
   )
